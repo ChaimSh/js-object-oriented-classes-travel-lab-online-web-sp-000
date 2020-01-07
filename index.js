2c695d34c8418fa6e6ba776fe9de5e7f39946690
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-01-07 00:40:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ class Driver{
   yearsExperienceFromBeginningOf(year){
     // number of years since drivers startDate
     // currentYear - startDate or driver.startDate
-    let currentDate = new Date(year, 01)
-    return currentDate - startDate
+    // let currentDate = new Date(year, 01)
+    // return currentDate - startDate
   }
 }
